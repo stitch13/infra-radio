b8e4a11b1fab66d0f82ad98496388e1a952c8eb3
Nelson M Neto <nelson.marques.neto@gmail.com>
2017-04-28 16:46:26 -0300

    Inicio do git

---
+++ infra-radio/resources/views/templates/cleanui/admin/home/home.blade.php
@@ -0,0 +1,20 @@
+@extends('templates.cleanui.admin.template')
+
+@section('content')
+
+<section class="page-content">
+    <div class="page-content-inner">
+
+        <!-- Dashboard -->
+        <div class="dashboard-container">
+
+            <div class="row">
+                <h1>Olá, Seja bem vindo !</h1>
+            </div>
+
+        </div>
+        <!-- End Dashboard -->
+
+    </div>
+</section>
+@endsection
---
+++ infra-radio/resources/views/templates/cleanui/admin/menu.blade.php
@@ -0,0 +1,90 @@
+<nav class="left-menu" left-menu>
+    <div class="logo-container">
+        <a href="../sys/home" class="logo">
+            <img src="../assets/common/img/logo.png" alt="Clean UI Admin Template" />
+            <img class="logo-inverse" src="../assets/common/img/logo-inverse.png" alt="Clean UI Admin Template" />
+        </a>
+    </div>
+    <div class="left-menu-inner scroll-pane">
+        <ul class="left-menu-list left-menu-list-root list-unstyled">
+            <li class="left-menu-list-active">
+                <a class="left-menu-link" href="../sys/home">
+                    <i class="left-menu-link-icon icmn-home2"><!-- --></i>
+                    <span class="menu-top-hidden">Home</span>
+                </a>
+            </li>
+            <li class="left-menu-list-submenu horizontal-left-direction">
+                <a class="left-menu-link">
+                    <i class="left-menu-link-icon icmn-home2"></i>
+                    <span class="menu-top-hidden">Configurações</span>
+                </a>
+                <!-- level 1 -->
+                <ul class="left-menu-list list-unstyled">
+                    <li class="left-menu-list-submenu">
+                        <a class="left-menu-link" href="javascript: void(0);">
+                            Usuarios
+                        </a>
+                        <!-- level 2 -->
+                        <ul class="left-menu-list list-unstyled">
+                            <a class="left-menu-link" href="#">
+                                Cadastro
+                            </a>
+                            <a class="left-menu-link" href="#">
+                                Grupos
+                            </a>
+                            <a class="left-menu-link" href="#">
+                                Permissoes
+                            </a>
+                        </ul>
+                    </li>
+                </ul>
+
+                <!-- level 1 -->
+                <ul class="left-menu-list list-unstyled">
+                    <li class="left-menu-list-submenu">
+                        <a class="left-menu-link" href="javascript: void(0);">
+                            Teste
+                        </a>
+                        <!-- level 2 -->
+                        <ul class="left-menu-list list-unstyled">
+                            <a class="left-menu-link" href="#">
+                                Cadastro
+                            </a>
+                            <a class="left-menu-link" href="#">
+                                Grupos
+                            </a>
+                            <a class="left-menu-link" href="#">
+                                Permissoes
+                            </a>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+
+<nav class="top-menu">
+    <div class="menu-icon-container hidden-md-up">
+        <div class="animate-menu-button left-menu-toggle">
+            <div><!-- --></div>
+        </div>
+    </div>
+    <div class="menu">
+        <div class="menu-user-block">
+            <div class="dropdown dropdown-avatar">
+                <a href="javascript: void(0);" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                    <span class="avatar" href="javascript:void(0);">
+                        <img src="../assets/common/img/temp/avatars/1.jpg" alt="Alternative text to the image">
+                    </span>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="" role="menu">
+                    <a class="dropdown-item" href="#"><i class="dropdown-icon icmn-user"></i> Profile</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="../logout"><i class="dropdown-icon icmn-exit"></i> Logout</a>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
